2622f76e7b1545cc8ba32dd4184c0c8146bc9f0c
rezky126 <rezky127@gmail.com>
2022-10-16 11:13:33 +0700

    created menu for Bottom Navigation View

--- app/src/main/java/com/example/stela_android/Homepage.kt
+++ app/src/main/java/com/example/stela_android/Homepage.kt
@@ -2,10 +2,34 @@ package com.example.stela_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_homepage.*
 
 class Homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        val bottomnav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomnav.itemIconTintList = null
+
+        val profile = Profile()
+
+        bottomNavigationView.setOnItemReselectedListener {
+            when(it.itemId){
+                R.id.btn_profile -> setCurrentFragment(profile)
+            }
+            true
+        }
     }
+
+
+    fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/stela_android/Profile.kt
@@ -0,0 +1,6 @@
+package com.example.stela_android
+
+import androidx.fragment.app.Fragment
+
+class Profile : Fragment(R.layout.activity_profile){
+}
