845483796c1684e9ef92e77046fe1746c3a9013b
gustonecrush <codeboxezz@gmail.com>
2022-10-17 20:36:31 +0700

    created hide and show tickets components

--- app/src/main/java/com/example/stela_android/ActiveTicketPage.kt
+++ app/src/main/java/com/example/stela_android/ActiveTicketPage.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_active_ticket_page.*;
 
 class ActiveTicketPage : AppCompatActivity() {
@@ -12,7 +14,12 @@ class ActiveTicketPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_active_ticket_page)
 
-        hideTicketsInformationSystem()
+        backBtnListener()
+
+        hideShowTicketsInformationSystem()
+        hideShowTicketsInfrastructureJaringan()
+        hideShowTicketsTataKelolaIT()
+        hideShowTicketsLainnya()
     }
 
     private fun backBtnListener() {
@@ -21,11 +28,53 @@ class ActiveTicketPage : AppCompatActivity() {
         }
     }
 
-    private fun hideTicketsInformationSystem() {
+    private fun hideShowTicketsInformationSystem() {
         dropdown_handler_1.setOnClickListener {
-            tickets_section_1.visibility = if(true) View.VISIBLE else View.GONE
-            dropdown_handler_1.setImageResource()
+            if(tickets_section_1.isVisible) {
+                tickets_section_1.visibility = View.GONE
+                dropdown_handler_1.setImageResource(R.drawable.ic_chevron_down_is)
+            } else  {
+                tickets_section_1.visibility = View.VISIBLE
+                dropdown_handler_1.setImageResource(R.drawable.ic_chevron_up_is)
+            }
         }
     }
+
+    private fun hideShowTicketsInfrastructureJaringan() {
+        dropdown_handler2.setOnClickListener {
+            if(tickets_section2.isVisible) {
+                tickets_section2.visibility = View.GONE
+                dropdown_handler2.setImageResource(R.drawable.ic_chevron_down_ij)
+            } else  {
+                tickets_section2.visibility = View.VISIBLE
+                dropdown_handler2.setImageResource(R.drawable.ic_chevron_up_ij)
+            }
+        }
+    }
+
+    private fun hideShowTicketsTataKelolaIT() {
+        dropdown_handler3.setOnClickListener {
+            if(tickets_section3.isVisible) {
+                tickets_section3.visibility = View.GONE
+                dropdown_handler3.setImageResource(R.drawable.ic_chevron_down_tkt)
+            } else  {
+                tickets_section3.visibility = View.VISIBLE
+                dropdown_handler3.setImageResource(R.drawable.ic_chevron_up_tkt)
+            }
+        }
+    }
+
+    private fun hideShowTicketsLainnya() {
+        dropdown_handler4.setOnClickListener {
+            if(tickets_section4.isVisible) {
+                tickets_section4.visibility = View.GONE
+                dropdown_handler4.setImageResource(R.drawable.ic_chevron_down_lainnya)
+            } else  {
+                tickets_section4.visibility = View.VISIBLE
+                dropdown_handler4.setImageResource(R.drawable.ic_chevron_up_lainnya)
+            }
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/stela_android/Homepage.kt
+++ app/src/main/java/com/example/stela_android/Homepage.kt
@@ -1,11 +1,21 @@
 package com.example.stela_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_homepage.*
 
 class Homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        ticketActiveBtnHandler()
+    }
+
+    fun ticketActiveBtnHandler() {
+        btn_ticket.setOnClickListener {
+            startActivity(Intent(this, ActiveTicketPage::class.java))
+        }
     }
 }
