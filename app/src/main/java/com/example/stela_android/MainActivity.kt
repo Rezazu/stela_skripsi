f04f831a7c08088947d3681dcec87a96bb2b09e3
rosyidrid <rosyidridho9g@gmail.com>
2022-10-17 14:15:38 +0700

    layout-form

--- app/src/main/java/com/example/stela_android/MainActivity.kt
+++ app/src/main/java/com/example/stela_android/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.stela_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.goTo)
+        button.setOnClickListener{
+            val intent = Intent(this, FormActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
