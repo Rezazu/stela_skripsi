335920541149aaee7e4efe55e7740c17abb62d91
gustonecrush <codeboxezz@gmail.com>
2022-10-17 09:04:26 +0700

    set back button on header

--- app/src/main/java/com/example/stela_android/ActiveTicketPage.kt
+++ app/src/main/java/com/example/stela_android/ActiveTicketPage.kt
@@ -1,11 +1,20 @@
 package com.example.stela_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_active_ticket_page.*;
 
 class ActiveTicketPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_active_ticket_page)
     }
+
+    private fun backBtnListener() {
+        back_btn.setOnClickListener {
+            startActivity(Intent(this, Homepage::class.java))
+        }
+    }
+
 }
