a7a0d63a1780712e4ad502579cbeac17ed7e3ea4
rezky126 <rezky127@gmail.com>
2022-10-26 15:01:02 +0700

    created form done activitiy, change button color, created home poster

---
+++ app/src/main/java/com/example/stela_android/FormDone.kt
@@ -0,0 +1,14 @@
+package com.example.stela_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_homepage.*
+
+class FormDone : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_done)
+    }
+}
